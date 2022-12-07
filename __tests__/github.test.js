4b3c94e7b071f584d2075319b2248f050cbaa1a5
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-12-07 10:10:47 -0800

    added passing test for api/v1/github/login

---
+++ __tests__/github.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('/api/v1/github/login should redirect to the github Oauth page', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
+  });
+  
+
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,15 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS users cascade;
+DROP TABLE IF EXISTS github_users cascade;
+DROP TABLE IF EXISTS posts cascade;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR,
+  email VARCHAR UNIQUE,
   password_hash VARCHAR NOT NULL
 );
 CREATE TABLE github_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    login TEXT NOT NULL,
+    login TEXT UNIQUE,
     email TEXT
+);
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR,
+  login TEXT,
+  content VARCHAR NOT NULL,
+  FOREIGN KEY (email) REFERENCES users(email),
+  FOREIGN KEY (login) REFERENCES github_users(login)
 );
